74b05728105997162500cb0debaab7ee6197bb0f
tabdo2048-alt <tabdo2048@gmail.com>
2026-09-21 04:55:40 +0300

    fix: restore virtual tour index route

--- src/routes/projects_.$slug.tour.$tourId.index.tsx
+++ src/routes/projects_.$slug.tour.$tourId.index.tsx
@@ -1,1 +1,23 @@
-fatal: path 'src/routes/projects_..tour..index.tsx' does not exist in 'HEAD'
+import { createFileRoute, notFound, redirect } from "@tanstack/react-router";
+import { selectDefaultTourScene, virtualTourQueryOptions } from "@/features/virtual-tour/queries";
+import { readRememberedTourScene } from "@/features/virtual-tour/tourSession";
+
+export const Route = createFileRoute("/projects_/$slug/tour/$tourId/")({
+  ssr: false,
+  loader: async ({ params, context }) => {
+    const bundle = await context.queryClient.ensureQueryData(
+      virtualTourQueryOptions(params.slug, params.tourId),
+    );
+    const rememberedSceneId = readRememberedTourScene(params.tourId);
+    const firstScene = bundle
+      ? (bundle.scenes.find((scene) => scene.id === rememberedSceneId && scene.is_published) ??
+        selectDefaultTourScene(bundle.scenes))
+      : null;
+    if (!bundle || !firstScene) throw notFound();
+    throw redirect({
+      to: "/projects/$slug/tour/$tourId/scene/$sceneId",
+      params: { slug: params.slug, tourId: params.tourId, sceneId: firstScene.id },
+      replace: true,
+    });
+  },
+});
